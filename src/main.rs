263d0a494257e9da0e814ae4c88ab678a9b573ed
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-04-16 15:03:34 +0300

    Change the code to collect all the data

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,131 @@
 use clap::Clap;
+use std::io::{self, BufRead, Write};
+use std::fs::{File, write, OpenOptions};
+use std::path::Path;
+use crate::utils::{read_lines, to_days, human, to_seconds};
+use std::str::FromStr;
+use std::num::ParseIntError;
+use crate::near::lockup_contract::VestingInformation;
 
 mod utils;
 mod near;
 
-#[derive(Clap, Debug, Clone)]
-#[clap(version = "0.2.0", author = "Near Inc. <hello@nearprotocol.com>")]
-pub(crate) struct Opts {
-    #[clap(short, long)]
-    pub lockup_account_id: String,
-    #[clap(short)]
-    pub block_height: Option<u64>,
+struct MonthTimestamp {
+    name: String,
+    fourteenth_timestamp: u64,
+    closest_block_height: u64,
+    closest_timestamp: u64,
+    finance_prediction: u64,
+}
+
+impl FromStr for MonthTimestamp {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coords: Vec<&str> = s.trim().split(',').collect();
+
+        Ok(MonthTimestamp {
+            name: coords[0].to_string(),
+            fourteenth_timestamp: coords[1].parse::<u64>()?,
+            closest_block_height: coords[2].parse::<u64>()?,
+            closest_timestamp: coords[3].parse::<u64>()?,
+            finance_prediction: coords[4].parse::<u64>()?,
+        })
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts: Opts = Opts::parse();
-    let account = opts.lockup_account_id;
-    let block = near::rpc::get_block(opts.block_height).await?;
+    let mut v: Vec<MonthTimestamp> = vec![];
+    for line in read_lines("timestamps.txt") {
+        v.push(line?
+            .trim()
+            .parse()
+            .expect("Something wrong with month timestamps"));
+    }
 
-    let block_height = block.height;
+    for month in v {
+        println!("{}", month.name);
 
-    println!("owner_account_id,account_id,initial_lockup_amount,current_locked_amount");
+        let block = near::rpc::get_block(Option::from(month.closest_block_height)).await?;
+        let filename = "lockups/".to_owned() + &month.name + ".txt";
 
-    loop {
-        match near::rpc::get_account_state(account.to_string(), block_height).await {
-            Ok(option_state) => {
-                if let Some(state) = option_state {
-                    println!("{:#?}", state);
-                    let locked_amount = state.get_locked_amount(block.timestamp).0;
-                    println!("{},{},{},{}",
-                        state.owner_account_id,
-                        account,
-                        utils::human(state.lockup_information.lockup_amount),
-                        utils::human(locked_amount)
-                    );
+        let res_filename = "res/".to_owned() + &month.name + ".txt";
+        let mut res_file = File::create(res_filename).expect("Can't create file");
+        // let mut res_file = OpenOptions::new().append(true).open(res_filename).expect("Can't open file");
+
+        for (i, line) in read_lines(filename).enumerate() {
+            let account_id = line.expect("Can't read line");
+
+            // data source. All that we have
+            let state = near::rpc::get_account_state(account_id.to_string(), block.height)
+                .await?
+                .expect(&("Can't get account details for ".to_owned() + &account_id));
+
+            // The amount of tokens locked for this account at the moment of deploying the contract.
+            let lockup_amount = state.lockup_information.lockup_amount;
+
+            // The amount of tokens that were withdrawn by NEAR foundation due to early termination
+            // of vesting.
+            // we are interested in it just to check do we need to take in into account
+            let termination = state.lockup_information.termination_withdrawn_tokens;
+
+            // We can't be sure in start of the contract, that's the main issue
+
+            // Size of cliff
+            let lockup_duration = state.lockup_information.lockup_duration;
+
+            // Size of lockup period after cliff
+            let release_duration = state.lockup_information.release_duration;
+            let human_release_duration = match release_duration {
+                None => { "empty_release_duration".to_string() }
+                Some(x) => { x.to_string() }
+            };
+
+            // Date when cliff ends. Could be none, unfortunately
+            // That could be our chance to detect start of contract
+            let lockup_timestamp = state.lockup_information.lockup_timestamp;
+            let human_lockup_timestamp = match lockup_timestamp {
+                None => { "empty_lockup_timestamp".to_string() }
+                Some(x) => { x.to_string() }
+            };
+
+            // we track and check transfers information inside get_account_state
+
+            // Contains all that we need, but it could be none or hashed
+            let vesting_info = match state.vesting_information {
+                VestingInformation::None => { "empty_vesting_info".to_string() }
+                VestingInformation::VestingHash(_) => { "vesting_hash".to_string() }
+                VestingInformation::VestingSchedule(ref s) => {
+                    format!("vesting_schedule({};{};{})",
+                            s.start_timestamp.0, s.cliff_timestamp.0, s.end_timestamp.0).to_string()
                 }
-                break;
-            },
-            Err(_err) => {},
+                VestingInformation::Terminating(ref t) => {
+                    format!("vesting_terminating({};{:?})", t.unvested_amount.0, t.status)
+                }
+            };
+
+            // On the one hand, it's the value from code from lockup contract
+            // on the other hand, I disagree with it
+            let locked_amount = state.get_locked_amount(block.timestamp).0;
+            // based on accs with opened vesting info, it does not take cliff into account
+
+            let a = format!("{},{},{},{},{},{},{},{},{}\n",
+                            account_id,
+                            lockup_duration,
+                            human_release_duration,
+                            lockup_amount,
+                            locked_amount,
+                            human_lockup_timestamp,
+                            vesting_info,
+                            termination,
+                            state.owner_account_id
+            );
+            res_file.write(a.as_bytes());
+
+            if i % 100 == 0 {
+                println!("{} ", i)
+            }
         }
     }
 
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,21 @@
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::fs::File;
+
 pub(crate) fn human(yocto: u128) -> u128 {
     yocto / 1000000000000000000000000
+}
+
+pub(crate) fn to_seconds(nanoseconds: u64) -> u64 {
+    nanoseconds / (1000 * 1000 * 1000)
+}
+
+pub(crate) fn to_days(nanoseconds: u64) -> u64 {
+    to_seconds(nanoseconds) / 86400
+}
+
+pub(crate) fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename).expect("File does not exist");
+    io::BufReader::new(file).lines()
 }
